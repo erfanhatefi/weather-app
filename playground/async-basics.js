cee8641a56ec63dd3ff9757575a72724baab4a6d
Randkill <erfan.vh@gmail.com>
2018-10-19 00:01:58 +0330

    basics of node JS Async + npm packages(request package)

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -5,4 +5,8 @@ setTimeout(function () {
     console.log('Inside of callback');
 } , 2000 ); //2000 msec to wait
 
+setTimeout(function () {
+    console.log('Zero/second timeout');
+} , 0);
+
 console.log('Finishing app');
