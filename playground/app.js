97c24ae7520b711ae37dc0cfa47b1f366e16685d
Randkill <erfan.vh@gmail.com>
2018-10-21 01:03:37 +0330

    new chnages been updated

--- playground/app.js
+++ playground/app.js
@@ -1,16 +1,24 @@
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-    //could not use Google`s API due to billings
-    //url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBWZifKyqkqlZSw-GGioVN1qbZmWKlj_yE&address=1301%20lombard%20street%20philadelphia%20",
-    url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
-    json: true
-} , function(error, response, body){        //argumetns are : error / response / body
-    //console.log(JSON.stringify(body, undefined, 2));
-    //console.log(JSON.stringify(response, undefined, 2));    
-    //console.log(JSON.stringify(error, undefined, 2));
-    console.log('address : ' + body.query.results.channel.title);
-    console.log('latitude : ' + body.query.results.channel.item.lat);
-    console.log('longtitude : ' + body.query.results.channel.item.long);
+const geocode = require('./geocode/geocode');
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
