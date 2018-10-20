11b2bf8f0f1c0acb77c1d9dcf1afdc97f39ae622
Randkill <erfan.vh@gmail.com>
2018-10-20 21:15:06 +0330

    test fetching JSON data from 'yahoo' weather API

--- playground/app.js
+++ playground/app.js
@@ -5,6 +5,12 @@ request({
     //url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBWZifKyqkqlZSw-GGioVN1qbZmWKlj_yE&address=1301%20lombard%20street%20philadelphia%20",
     url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
     json: true
-} , function(error, reponse, body){        //argumetns are : error / response / body
-    console.log(JSON.stringify(body, undefined, 2));
+} , function(error, response, body){        //argumetns are : error / response / body
+    //console.log(JSON.stringify(body, undefined, 2));
+    //console.log(JSON.stringify(response, undefined, 2));    
+    //console.log(JSON.stringify(error, undefined, 2));
+    console.log('address : ' + body.query.results.channel.title);
+    console.log('latitude : ' + body.query.results.channel.item.lat);
+    console.log('longtitude : ' + body.query.results.channel.item.long);
+
 });
