82680ce259a74e4c8b3c657aceb7144b18750494
Randkill <erfan.vh@gmail.com>
2018-10-28 16:44:20 +0330

    10/28 update

--- playground/app.js
+++ playground/app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 
 const argv = yargs
   .options({
@@ -17,10 +18,22 @@ const argv = yargs
 
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
-    console.log("ERROR");
     console.log(errorMessage);
   } else {
-    console.log("RESULTS:");
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('address is : ');
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longtitude, function(errorMessage, weatherResults){
+      if(errorMessage){
+        console.log(errorMessage);
+      }
+      else{
+        console.log('It`s currently ' + weatherResults.temperature + ' . It feels like '+ weatherResults.apparentTemperature);
+      }
+    });
   }
 });
+
+//API key :     77be46c3bb15d417320755805d721c00
+
+//new york       lat: 40.71455        lng : -74.007118
+//overall for new york : https://api.darksky.net/forecast/77be46c3bb15d417320755805d721c00/40.71455,-74.007118
